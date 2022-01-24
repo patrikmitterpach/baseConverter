1145924c12bba570e647570167d700b16dd1f96e
Mitterpach <patrik@mitterpach.com>
2022-01-24 11:47:16 +0100

    Add comments to loop and variables

--- baseConverter.py
+++ baseConverter.py
@@ -24,11 +24,15 @@ while True:
         
 
 # Variables
+#  To expand base selection, edit 
+#  possibleModulos to include the needed selection.
 returnArray = []
 possibleModulos = ['0','1','2','3','4','5','6','7','8','9',
                     'A','B','C','D','E','F']
 
-# Loop:
+# Loop
+#   Basic algorithm to save to modulos of division by the base
+#   to a separate array, which is then returned in reverse order.
 while number > 0:
     modulo = int(number % inputBase)
     returnArray.append( possibleModulos[modulo] )
