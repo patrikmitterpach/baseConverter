acb2cbe5da96baff83bec911274a5ac2a5d092fe
Mitterpach <patrik@mitterpach.com>
2022-01-24 11:42:57 +0100

    Add consistent camelCase

--- baseConverter.py
+++ baseConverter.py
@@ -24,19 +24,19 @@ while True:
         
 
 # Variables
-return_array = []
-possible_modulos = ['0','1','2','3','4','5','6','7','8','9',
+returnArray = []
+possibleModulos = ['0','1','2','3','4','5','6','7','8','9',
                     'A','B','C','D','E','F']
 
 # Loop:
 while number > 0:
-    modulo = int(number%inputBase)
-    return_array.append(possible_modulos[modulo])
-    number = (number-modulo) / inputBase
+    modulo = int(number % inputBase)
+    returnArray.append( possibleModulos[modulo] )
+    number = (number - modulo) / inputBase
  
 # Output   
-return_array.reverse()
-return_number = ''.join(map(str,return_array))
+returnArray.reverse()
+returnNumber = ''.join( returnArray )
 
-print(return_number)
+print(returnNumber)
 
